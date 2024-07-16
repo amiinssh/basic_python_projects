8612bf9c735410d95769520f859b08e48d1665ab
amiinssh <145289316+amiinssh@users.noreply.github.com>
2024-07-15 18:59:33 -1200

    Add files via upload

---
+++ bandNameGenerator.py
@@ -0,0 +1,7 @@
+print("welcome to  my band generator")
+
+name_of_city = input("enter the name of the city you grew up in: ")
+
+name_of_pet = input("enter the name of your favorite pet: ")
+
+print(f"The name of your band is {name_of_pet} {name_of_city}\n")

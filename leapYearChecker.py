2e341cd4d62b489d8690e59930d2895577191d63
amiinssh <145289316+amiinssh@users.noreply.github.com>
2024-07-18 18:54:35 -1200

    Add files via upload

---
+++ leapYearChecker.py
@@ -0,0 +1,7 @@
+year = int(input("Which year do you want to check? "))
+
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+    print(f"The year {year} is a leap year.")
+
+else:
+        print(f"The year {year} is not a leap year.")

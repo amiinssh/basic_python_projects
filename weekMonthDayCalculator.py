dfce36836ae19442ce17ddef61fac56258c07b2c
amiinssh <145289316+amiinssh@users.noreply.github.com>
2024-07-18 17:32:08 -1200

    Add files via upload

---
+++ weekMonthDayCalculator.py
@@ -0,0 +1,11 @@
+age = int(input("what is your age?\n"))
+
+age = 80 - age
+
+months = age * 12
+
+weeks = age * 52
+
+days = months * 30
+
+print(f"your remaining months are {months} your remaining weeks are {weeks} and your remaining days are {days}")
